20bce682d45dcc129b0cd8abc4d109db041dab10
Arvind Chembarpu <achembarpu@gmail.com>
2017-02-08 13:05:41 +0530

    Normalize url building

--- pockyt/api.py
+++ pockyt/api.py
@@ -8,24 +8,29 @@ class API(object):
 
     APP_NAME = 'pockyt'
 
-    POCKET_URL = 'https://getpocket.com/'
-    API_URL = POCKET_URL + 'v3/'
+    POCKET_URL = 'https://getpocket.com'
+    API_URL = POCKET_URL + '/v3'
 
     REDIRECT_URL = 'https://www.github.com/arvindch/{0}'.format(APP_NAME)
     ISSUE_URL = REDIRECT_URL + '/issues/new'
     APP_CREATE_URL = 'http://getpocket.com/developer/apps/new'
 
-    REQUEST_TOKEN_URL = API_URL + 'oauth/request'
-    ACCESS_TOKEN_URL = API_URL + 'oauth/authorize'
+    REQUEST_TOKEN_URL = API_URL + '/oauth/request'
+    ACCESS_TOKEN_URL = API_URL + '/oauth/authorize'
 
-    RETRIEVE_URL = API_URL + 'get'
-    MODIFY_URL = API_URL + 'send'
+    RETRIEVE_URL = API_URL + '/get'
+    MODIFY_URL = API_URL + '/send'
 
     CONFIG_HEADER = 'CREDENTIALS'
+
+    ENCODING = 'utf-8'
+    DECODING = 'unicode_escape'
+
+    CONTENT_TYPE = 'application/json'
 
     INFO_KEYS = ['id', 'title', 'link', 'excerpt', 'tags']
 
     @classmethod
     def get_auth_user_url(cls, rt):
-        return cls.POCKET_URL + 'auth/authorize?request_token={0}' \
+        return cls.POCKET_URL + '/auth/authorize?request_token={0}' \
                                 '&redirect_uri={1}'.format(rt, cls.REDIRECT_URL)
